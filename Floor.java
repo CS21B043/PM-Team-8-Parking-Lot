864afb038a9a8ea6d2beb202984d4792d6102ab8
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-09-18 19:11:53 +0530

    Update Floor.java

--- Floor.java
+++ Floor.java
@@ -1,7 +1,12 @@
+import java.util.Date;
+
 public interface Floor {
     boolean isFull();
     void slotsAvailable();
     void display_entry_points();
     void display_exit_points();
     void display_reserved_spots();
+    Vehicle findVehicle(int slotno);
+    Date findDate(int slotno);
+    void clearSpots(int slotno);
 }
