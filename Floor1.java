12b4c3631b648f7e5783d8f281b4ce8ad3b53d04
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-09-19 15:58:39 +0530

    spots available changed from int to boolean type

--- Floor1.java
+++ Floor1.java
@@ -6,16 +6,15 @@ public class Floor1 implements Floor{
 
     /** This floor is to park all non-electric 4 wheelers
      * Has 200 parking spots, out of which the first 20 are reserved for handicapped
-     * We have a List named spot which has initially values 0s and 1s ( 0- general free spots, 1- handicapped free spots)
      */
 
     List<ParkingSpot> spots = new ArrayList<>();
     final int MAX_SPOTS = 200;
-    int[] spots_available =new int[MAX_SPOTS];
+    boolean[] spots_available =new boolean[MAX_SPOTS];
 
     private void farr(){
         for(int i=0; i<MAX_SPOTS; ++i){
-            spots_available[i] = 1;
+            spots_available[i] = true;
         }
     }
     public Floor1() {
