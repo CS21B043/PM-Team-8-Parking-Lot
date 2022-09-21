d13fc0856931298778e4f64e023348e77badb224
CS21B043 <110807261+CS21B043@users.noreply.github.com>
2022-09-21 21:32:36 +0530

    Updated Floor 2 methods

--- Floor2.java
+++ Floor2.java
@@ -9,8 +9,8 @@ public class Floor2 implements Floor{
      *
      */
 
-    List<ParkingSpot> spots = new ArrayList<>();
     final int MAX_SPOTS = 160;
+    List<ParkingSpot> spots = new ArrayList<>(MAX_SPOTS);
     boolean[] spots_available =new boolean[MAX_SPOTS];
 
     private void initialize_spots_available(){
@@ -18,35 +18,64 @@ public class Floor2 implements Floor{
             spots_available[i] = true;
         }
     }
+    private void initialize_array_list(){
+        for(int i=0; i<MAX_SPOTS; ++i){
+            spots.add(i, null);
+        }
+    }
 
     public Floor2(){
         initialize_spots_available();
+        initialize_array_list();
     }
 
     @Override
     public boolean isFull() {
-        return false;
+        boolean k = true;
+        for(int i=0; i<MAX_SPOTS; ++i){
+            if(spots_available[i]){
+                k = false;
+                break;
+            }
+        }
+        return k;
     }
 
 
     @Override
     public void slotsAvailable() {
-
+        System.out.println("GENERAL PARKING SLOTS");
+        for(int i=12; i<MAX_SPOTS; ++i){
+            if(spots_available[i]){
+                System.out.print(i+1 + " ");
+            }
+        }
     }
 
     @Override
     public void display_entry_points() {
-
+        System.out.println("1. Handicapped Road");
+        System.out.println("2. South Gate Road");
+        System.out.println("3. North Gate Road");
+        System.out.println("4. East Gate Road");
     }
 
     @Override
     public void display_exit_points() {
-
+        System.out.println("1. Handicapped Road");
+        System.out.println("2. South Gate Road");
+        System.out.println("3. North Gate Road");
+        System.out.println("4. East Gate Road");
     }
 
     @Override
     public void display_reserved_spots() {
-
+        System.out.println("RESERVED FOR HANDICAPPED");
+        for(int i=0; i<12; ++i){
+            if(spots_available[i]){
+                System.out.print(i+1 + " ");
+            }
+        }
     }
 
     @Override
@@ -56,21 +85,28 @@ public class Floor2 implements Floor{
 
     @Override
     public Date findDate(int slotno) {
-        return null;
+        return spots.get(slotno-1).getEntry();
     }
 
     @Override
     public void clearSpots(int slotno) {
-
+        spots_available[slotno - 1] = true;
     }
 
     @Override
     public void add_vehicle(ParkingSpot p) {
-
+        int slotno = p.getSlotNo();
+        if(spots_available[slotno-1]){
+            spots_available[slotno-1] = false;
+            spots.set(slotno-1,p);
+        }
     }
 
     @Override
     public void vehicle_exit(int slotno) {
-
+        if(!spots_available[slotno-1]){
+            spots_available[slotno-1] = true;
+            spots.set(slotno-1,null);
+        }
     }
 }
