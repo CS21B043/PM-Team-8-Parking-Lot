9285166cc93564adf50720f2fb18434d366fca9f
CS21B043 <110807261+CS21B043@users.noreply.github.com>
2022-09-24 12:06:52 +0530

    Added comments

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
+    /* Creating a menu using which the user can enter a vehicle, know the parking fee, know the available slots, pay and exit his vehicle etc...
     public static void menu(){
         System.out.println("\nMenu:");
         System.out.println("1)Select Ticket counter");
@@ -16,11 +17,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        //Declaring an integer using which the user can navigate the menu.
         int n;
+        //Creating an object of the TicketCounter class.
         TicketCounter ticket_counter = new TicketCounter();
         while(true){
             menu();
             n = in.nextInt();
+            /*If n=1, then the user can choose the tc and enter their vehicle details to get a list of available parking slots from which they can choose
+            the slot they like*/
             if(n == 1){
                 System.out.println("1)TC - 1");
                 System.out.println("2)TC - 2");
@@ -57,15 +62,19 @@ public class Main {
                 ticket_counter.availableSpots(v);
 
             }
+            //If n=2, then the user can get to know the per hour parking charges, based on which they can choose whether or not to park.
             else if (n == 2){
                 ticket_counter.display_parking_charges();
             }
+            //If n=3, then the user can get to know all the available parking slots in the building.
             else if (n == 3){
                 ticket_counter.display_free_slots();
             }
+            //If n=4, then the user can get to know the list of vehicles parked in the building.
             else if (n == 4){
                 ticket_counter.display_vehicles();
             }
+            //If n=5, then the user can exit by entering the slot number and the floor number on which their vehicle was parked.
             else if (n == 5){
 
                 /** Get slot no, and floor.no and show the price for parking, get the cash paid through
@@ -77,6 +86,7 @@ public class Main {
                 int floorno = in.nextInt();
                 ticket_counter.exit_vehicle(slotno, floorno);
             }
+            //If, n=6, the program terminates.
             else if(n == 6){
                 break;
             }
